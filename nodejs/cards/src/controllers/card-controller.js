2251f3856519e67f7a657e0aae61498d5cd8e5e6
DevEsteves <Esteveshxc@outlook.com>
2021-01-23 15:13:44 -0300

    Prova nodejs

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -2,6 +2,30 @@ const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+    neDB.find({}).sort({ id: 1 }).exec((exception, cards) => {
+        if (exception) {
+            const sentence = 'Deu ruim na tentativa de listar todos os Cards!'
+            console.log(sentence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.json(cards)
+    })
 }
+
+api.save = (request, response) => {
+const canonical = request.body
+    neDB.insert(canonical, (exception, cards) => {
+        if (exception) {
+            const sentence = 'Deu ruim na tentativa de listar todos os Cards!'
+            console.log(sentence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': sentence })
+        }
+        response.status(201)
+        response.json(cards)
+    })
+}
+
 
 module.exports = api
