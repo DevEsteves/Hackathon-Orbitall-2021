8ee4edfeae3390c422ecc48532cd681b0845dd8a
DevEsteves <Esteveshxc@outlook.com>
2021-01-30 18:51:19 -0300

    adding delete and update changes

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -13,6 +13,18 @@ api.findAll = (request, response) => {
     })
 }
 
+api.findById = function (request, response) {
+    const id = request.params.id
+    neDB.findOne({ _id: id }, function (exception, card) {
+        if (exception) {
+            jsonAsError = { message: exception }
+            response.json(jsonAsError)
+            response.status(exception.status | 501).end()
+        }
+        response.json(card)
+    })
+}
+
 api.save = (request, response) => {
 const canonical = request.body
     neDB.insert(canonical, (exception, cards) => {
@@ -27,5 +39,60 @@ const canonical = request.body
     })
 }
 
+api.update = function (request, response) {
+    const primaryKey = request.params.id
+    const canonical = request.body
+    neDB.update({ _id: primaryKey }, canonical, (exception, hadUpdated) => {
+        if (exception) {
+            jsonAsError = { message: exception }
+            response.json(jsonAsError)
+            response.status(exception.status | 501).end()
+        }
+        if (hadUpdated) {
+            canonical._id = primaryKey
+            response.json(canonical)
+        } else {
+            jsonAsError = { message: exception }
+            response.json(jsonAsError)
+            response.status(exception.status | 501).end()
+        }
+    })
+}
+
+api.delete = function (request, res) {
+    const primaryKey = request.params.id
+    neDB.remove({ _id: primaryKey }, {}, (exception, hadDeleted) => {
+        if (exception) {
+            jsonAsError = { message: exception }
+            response.json(jsonAsError)
+            response.status(exception.status | 501).end()
+        }
+        if (hadDeleted) {
+            res.json({ 'message': `Card id '${primaryKey}' had been deleted successfuly` })
+            res.status(200)
+        } else {
+            jsonAsError = { message: exception }
+            response.json(jsonAsError)
+            response.status(exception.status | 501).end()
+        }
+    })
+}
+
+api.paginationAndSorting = (request, response) => {
+    const page = request.query.page
+    const limit = request.query.limit
+
+    console.log(`page ${page}`)
+    console.log(`limit ${limit}`)
+
+    neDB.find({}).sort({ name: 1 }).skip(page).limit(limit).exec((exception, cards) => {
+        if (exception) {
+            jsonAsError = { message: exception }
+            response.json(jsonAsError)
+            response.status(exception.status | 501).end()
+        }
+        response.json(cards)
+    })
+}
 
 module.exports = api
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -4,5 +4,12 @@ module.exports = (app) => {
     app.route('/cards')
         .get(api.findAll)
         .post(api.save)
-        
+    
+    app.route('/cards/:id')
+    .get(api.findById)
+    .put(api.update)
+    .delete(api.delete)
+
+    app.route('/card/paginationandSorting')
+        .get(api.paginationAndSorting) 
 }
